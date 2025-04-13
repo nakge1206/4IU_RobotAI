93d452e103c8f27e3e6646d8add58bc6fea293f7
nakge <kogwanho3@gmail.com>
2025-04-13 23:28:16 +0900

    stt_test com

---
+++ rtAPI_main_test.py
@@ -0,0 +1,18 @@
+# service.py
+from rtAPI_stt_test import STTClient
+from rtAPI_tts_test import TTSClient
+import time
+
+def main():
+    tts = TTSClient()
+    tts.connect()
+
+    def handle_stt_text(text):
+        print(f"🎤 STT 인식 결과: {text}")
+        tts.send_text(text)
+
+    stt = STTClient(on_text_callback=handle_stt_text)
+    stt.start()
+
+if __name__ == "__main__":
+    main()
