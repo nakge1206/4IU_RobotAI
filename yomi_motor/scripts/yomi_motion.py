3dd5955c650e7c24c5c42262c1fe54981964e9f4
jgb08191 <vnddns2134@gmail.com>
2025-08-03 17:40:35 +0900

    motion2

--- yomi_motor/scripts/yomi_motion.py
+++ yomi_motor/scripts/yomi_motion.py
@@ -5,7 +5,7 @@ from yomi_motor import MotionSequenceExecutor
 class MotionController:
     def __init__(self, vision = None):
         rospy.init_node('motion_controller_node', anonymous=True)
-        self.executor = MotionSeq uenceExecutor()
+        self.executor = MotionSequenceExecutor()
         self.vision = vision
 
     def stand(self):
@@ -42,13 +42,13 @@ class MotionController:
         "가까운 사용자에게 다가간다."
 
     def I_trust1(self):
-        "신뢰하는 사용자 옆에서 따라다님"
+        "/신뢰하는 사용자 옆에서 따라다님"
 
     def I_trust2(self):
-        "사용자 지시가 있으면 즉각 응답 및 행동을 수행"
+        "/사용자 지시가 있으면 즉각 응답 및 행동을 수행"
 
     def I_trust3(self):
-        "행동 및 이벤트 뒤 사용자 쪽으로 돌아옴"
+        "/행동 및 이벤트 뒤 사용자 쪽으로 돌아옴"
 
     def I_fear1(self):
         "(위협 감지시)모서리로 움직인다 벽을 보고 선다"
@@ -60,7 +60,7 @@ class MotionController:
         "큰 물건 뒤로 숨는다"
 
     def I_surprise1(self):
-        "(소리-(어? 뭐지?) 작은 목소리 출력 후) 바퀴가 뒤로 살짝 밀리며 정지"
+        "/(소리-(어? 뭐지?) 작은 목소리 출력 후) 바퀴가 뒤로 살짝 밀리며 정지"
 
     def I_surprise2(self):
         "미세 움직임으로 진동 표현, 떨림 표현"
@@ -73,7 +73,7 @@ class MotionController:
             rospy.sleep(1.5)
 
     def I_surprise3(self):
-        "((호불호에서 호의 경우) 대상으로부터 가까워지고, (불호의 경우) 대상으로부터 멀어짐)"
+        "/((호불호에서 호의 경우) 대상으로부터 가까워지고, (불호의 경우) 대상으로부터 멀어짐)"
 
     def I_sadness1(self):
         "느리게 이동"
@@ -89,7 +89,7 @@ class MotionController:
         rospy.sleep(1.5)
 
     def I_disgust1(self):
-        "원치 않는 사용자 접근 시 후진"
+        "/원치 않는 사용자 접근 시 후진"
 
     def I_disgust2(self):
         "회피 루트를 탐색"
@@ -117,7 +117,7 @@ class MotionController:
         self.executor.motor_publisher_batch(motor_ids=[11], motor_positions=[220], motor_speeds=[4])
 
     def E_joy1(self):
-        "(행위가 끝난뒤) 크게 공간을 한바퀴 돌며 정해진 위치로 돌아감"
+        "/(행위가 끝난뒤) 크게 공간을 한바퀴 돌며 정해진 위치로 돌아감"
     
     def E_joy2(self):
         "정위치에서 양손을 든다."
@@ -128,16 +128,83 @@ class MotionController:
         "바퀴로 신나게 빙빙 돈다"
 
     def E_trust1(self):
-        "신뢰대상에게 일정거리 유지하며 옆에 선다."
+        "/신뢰대상에게 일정거리 유지하며 옆에 선다."
     
     def E_trust2(self):
         "일정거리 유지하며 따라다닌다."
 
     def E_trust3(self):
-        "사용자 지시가 있으면 즉각 응답 및 행동을 수행하고 대기한다."
+        "/사용자 지시가 있으면 즉각 응답 및 행동을 수행하고 대기한다."
     
     def E_fear1(self):
         "(위협감지 시)모서리로 움직인다 벽을 보고 선다."
+    
+    def E_fear2(self):
+        "신뢰하는 사용자 뒤로 숨는다."
+    
+    def E_fear3(self):
+        "큰 물건 뒤로 숨는다"
+
+    def E_surprise1(self):
+        "/(소리-(어? 뭐지?) 작은 목소리 출력 후) 바퀴가 뒤로 살짝 밀리며 정지"
+
+    def E_surprise2(self):
+        "미세 움직임으로 진동 표현, 떨림 표현"
+        self.executor.motor_publisher_batch(motor_ids=[6, 10], motor_positions=[95, 265], motor_speeds=[5])
+        rospy.sleep(0.2)
+        for i in range(3):
+            self.executor.motor_publisher_batch(motor_ids=[5, 9], motor_positions=[90, 270], motor_speeds=[4, 4])
+            rospy.sleep(1.5)
+            self.executor.motor_publisher_batch(motor_ids=[5, 9], motor_positions=[200, 160], motor_speeds=[4, 4])
+            rospy.sleep(1.5)
+
+    def E_surprise3(self):
+        "/((호불호에서 호의 경우) 대상으로부터 가까워지고, (불호의 경우) 대상으로부터 멀어짐)"
+
+    def E_sadness1(self):
+        "느리게 이동"
+
+    def E_sadness2(self):
+        "고개를 숙이듯 머리가 아래로 향함"
+        self.executor.motor_publisher_batch(motor_ids=[11], motor_positions=[159], motor_speeds=[4])
+        rospy.sleep(1.5)
+
+    def E_sadness3(self):
+        "벽 쪽에 멈춰서 혼자 조용히 있기"
+
+    def E_disgust1(self):
+        "원치 않은 사용자 접근 시 후진"
+
+    def E_disgust2(self):
+        "옆으로 돌아서기(고개를 돌리는 것과 같은 효과 일거 같아서 그렇게 만듬)"
+        self.executor.motor_publisher_batch(motor_ids=[12], motor_positions=[240], motor_speeds=[4])
+        rospy.sleep(1.5)
+
+    def E_disgust3(self):
+        "도망가기"
+    
+    def E_anger1(self):
+        "본체의 바퀴가 빠르게 앞뒤로 움직임"
+
+    def E_anger2(self):
+        "몸 전체에 진동(가능하면)"
+        for i in range(5):
+            self.executor.motor_publisher_batch(motor_ids=[6, 10], motor_positions=[95, 265], motor_speeds=[7])
+            self.executor.motor_publisher_batch(motor_ids=[6, 10], motor_positions=[98, 262], motor_speeds=[7])
+
+    def E_anger3(self):
+        "주변을 빠르게 돌아다닌다"
+
+    def E_anticipation1(self):
+        "주변을 돌아다닌다"
+
+    def E_anticipation2(self):
+        "바퀴로 자리에서 빙글빙글 천천히 돈다"
+    
+    def E_anticipation3(self):
+        "새로운 입력을 기다리는 듯 정지 후 전방 주시"
+        self.executor.motor_publisher_batch(motor_ids=[11], motor_positions=[220], motor_speeds=[4])
+    
 
     def move_to_greeting_pose(self):
         # 5번 모터 위치로 팔을 올리고 서보로 손 흔들기
@@ -159,9 +226,28 @@ class MotionController:
             rate.sleep()
 
 if __name__ == '__main__':
+    # try:
+      #  controller = MotionController()
+      #  controller.I_joy1()
+      #  rospy.spin()
+    # except rospy.ROSInterruptException:
+      #  pass
     try:
         controller = MotionController()
-        controller.I_joy1()
-        rospy.spin()
+        while not rospy.is_shutdown():
+            func_name = input("함수이름(종료는 end) ")
+            if func_name.lower() == 'end':
+                break
+            elif hasattr(controller, func_name):
+                func = getattr(controller, func_name)
+                if callable(func):
+                    try:
+                        func()
+                    except Exception as e:
+                        print(f"함수 실행 중 오류 발생: {e}")
+                else:
+                    print("호출 가능한 함수가 아님.")
+            else:
+                print("해당하는 함수 x")
     except rospy.ROSInterruptException:
         pass
